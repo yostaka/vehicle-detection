9c7357d63f0a95d4669f9de3951621d3f765db55
yostaka <ytacka@gmail.com>
2018-06-11 16:56:38 +0900

    add window searching with multiple sizes

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ test_imgs_output_folder = './output_images/test_images/'
 test_imgs = glob.glob('./test_images/test*.jpg')
 cars_imgs = glob.glob('./train_data/vehicles/**/*.png')
 notcars_imgs = glob.glob('./train_data/non-vehicles/**/*.png')
-sample_size = 1000
+sample_size = 500
 
 runSampleHOGFeatureExtraction = False
 
@@ -146,29 +146,46 @@ print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
 t = time.time()
 
 
-image = mpimg.imread('./test_images/test1.jpg')
-draw_image = np.copy(image)
+images = glob.glob('./test_images/test*.jpg')
+# image = mpimg.imread('./test_images/test1.jpg')
+# draw_image = np.copy(image)
 
-# Uncomment the following line if you extracted training
-# data from .png images (scaled 0 to 1 by mpimg) and the
-# image you are searching is a .jpg (scaled 0 to 255)
+image = mpimg.imread(images[0])
 image = image.astype(np.float32)/255
 
-windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop,
-                       xy_window=(96, 96), xy_overlap=(0.5, 0.5))
+xy_windows = [(80, 80), (96, 96), (108, 108), (120, 120)]
+windows = []
 
-hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space,
-                             spatial_size=spatial_size, hist_bins=hist_bins,
-                             orient=orient, pix_per_cell=pix_per_cell,
-                             cell_per_block=cell_per_block,
-                             hog_channel=hog_channel, spatial_feat=spatial_feat,
-                             hist_feat=hist_feat, hog_feat=hog_feat)
+for xy_window in xy_windows:
+    window = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop,
+                          xy_window=xy_window, xy_overlap=(0.6, 0.6))
+    windows.extend(window)
 
-window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
 
-plt.figure()
-plt.imshow(window_img)
-plt.show()
+for fname in images:
+    image = mpimg.imread(fname)
+    draw_image = np.copy(image)
+
+    # Uncomment the following line if you extracted training
+    # data from .png images (scaled 0 to 1 by mpimg) and the
+    # image you are searching is a .jpg (scaled 0 to 255)
+    # image = image.astype(np.float32)/255
+    #
+    # windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop,
+    #                        xy_window=(140, 140), xy_overlap=(0.2, 0.2))
+
+    hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space,
+                                 spatial_size=spatial_size, hist_bins=hist_bins,
+                                 orient=orient, pix_per_cell=pix_per_cell,
+                                 cell_per_block=cell_per_block,
+                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                 hist_feat=hist_feat, hog_feat=hog_feat)
+
+    window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
+
+    plt.figure()
+    plt.imshow(window_img)
+    plt.show()
 
 # Run pipeline on a video stream (start with the test_video.mp4 and later implement on full project_video.mp4)
 # and create a heat map of recurring detections frame by frame to reject outliers and follow detected vehicles
